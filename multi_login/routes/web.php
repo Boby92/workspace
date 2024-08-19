96a6bc908d968566f049bc62f51a7e7b9b2d439b
Boby92 <boban.mladenovic92@gmail.com>
2024-08-19 17:34:34 +0200

    Update web.php

--- multi_login/routes/web.php
+++ multi_login/routes/web.php
@@ -22,10 +22,6 @@ Route::get('/seller/dashboard', function () {
     return view('seller.dashboard');
 })->middleware(['auth', 'verified', 'seller'])->name('seller.dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
